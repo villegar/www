5624fffad7ac799f1c593a4cd91947d04067f2b0
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-12 23:16:24 -0500

    Minor changes

--- R/build.R
+++ R/build.R
@@ -1,3 +1,3 @@
-knitr::opts_knit$set(root.dir = 'static/presentations')
+#knitr::opts_knit$set(root.dir = 'static/presentations')
 #rmarkdown::render("./2020-07/index.Rmd")
-blogdown::build_dir(".")
+blogdown::build_dir("static/presentations")
